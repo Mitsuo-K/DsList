6525086ddb16e5005a47d24c882a83e40e6a84d6
MitsuoK <mitsuo.kubo01@gmail.com>
2023-07-12 20:03:59 -0300

    Project created

---
+++ src/test/java/com/myzum/BackendJava/BackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myzum.BackendJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

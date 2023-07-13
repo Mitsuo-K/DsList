a5ede1845f98ab9358cf487b1137fbfc78c58186
MitsuoK <mitsuo.kubo01@gmail.com>
2023-07-12 21:20:32 -0300

    Game repository,service,controller

---
+++ src/main/java/com/myzum/BackendJava/controllers/GameController.java
@@ -0,0 +1,25 @@
+package com.myzum.BackendJava.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myzum.BackendJava.dto.GameMinDTO;
+import com.myzum.BackendJava.entities.Game;
+import com.myzum.BackendJava.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+
+    @Autowired
+    private GameService gameService;
+
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        return gameService.findAll();
+    }
+}
---
+++ src/main/java/com/myzum/BackendJava/services/GameService.java
@@ -0,0 +1,23 @@
+package com.myzum.BackendJava.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.myzum.BackendJava.dto.GameMinDTO;
+import com.myzum.BackendJava.entities.Game;
+import com.myzum.BackendJava.repositories.GameRepository;
+
+@Service
+public class GameService {
+
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+        List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+}
